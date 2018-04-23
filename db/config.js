37e3c5690d4451ce2f63035d301f8964c1bcf487
Muhammad Ahmed <ahmede7th@gmail.com>
2018-04-23 11:13:36 -0400

    heroku

--- db/config.js
+++ db/config.js
@@ -9,9 +9,9 @@ const pgp = require('pg-promise')(options)
 function setDatabase(){
   if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV){
     return pgp({
-      database: 'benzdb',
-      port: 5432,
-      host: 'localhost'
+      database: process.env.PG_DATABASE || 'benzdb',
+      port: process.env.PG_PORT || 5432,
+      host: process.env.PG_HOST || 'localhost'
     })
 
   } else if (process.env.NODE_ENV === 'production'){
